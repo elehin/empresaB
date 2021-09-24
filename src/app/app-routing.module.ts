08254d937e2737b1090d706207fd07ec1623614b
elehin <32306659+elehin@users.noreply.github.com>
2021-09-24 19:20:19 +0200

    Contacto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CatalogComponent } from './catalog/catalog.component';
+import { ContactoComponent } from './contacto/contacto.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: 'catalog', component: CatalogComponent }
+  { path: 'catalog', component: CatalogComponent },
+  { path: 'contacto', component: ContactoComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { CatalogComponent } from './catalog/catalog.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     CatalogComponent,
+    ContactoComponent,
   ],
   imports: [
     BrowserModule,
